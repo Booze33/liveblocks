4c011ba4ae0e02baa55c06e177383d549101e6ad
Booze33 <tebetisloh@gmail.com>
2024-07-22 10:19:59 +0200

    Add editor

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,48 @@
+import Header from '@/components/Header'
+import AddDocumentBtn from '@/components/AddDocumentBtn'
 import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 import React from 'react'
+import { redirect } from 'next/navigation'
+import { EmailAddress, currentUser } from '@clerk/nextjs/server'
 
-const Home = () => {
+const Home = async () => {
+  const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
+
+  const documents = [];
+
   return (
-    <div>
-      <Button>
-        Button
-      </Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap=4">
+          Notification
+          <SignedIn>
+            <UserButton  />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {documents.length > 0 ? (
+        <div></div>
+      ): (
+        <div className='document-list-empty'>
+          <Image
+            src="/assets/icons/doc.svg"
+            alt="Document"
+            width={40}
+            height={40}
+            className="mx-auto"
+          />
+
+          <AddDocumentBtn
+            userId={clerkUser.id}
+            email={clerkUser.emailAddresses[0].emailAddress}
+          />
+        </div>
+      )}
+    </main>
   )
 }
 
